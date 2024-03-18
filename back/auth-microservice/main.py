ea97a810af9f6570f4fadf58169c5bc61290b419
Juan Pablo Rincon Usma <68928340+juan9572@users.noreply.github.com>
2024-03-18 14:40:06 -0500

    Update main.py

--- back/auth-microservice/main.py
+++ back/auth-microservice/main.py
@@ -1,24 +1,46 @@
-from models import Usuario
+from pydantic import BaseModel
 from decouple import config
 from sqlalchemy import create_engine
+from models import Users, Permissions
 from sqlalchemy.orm import sessionmaker
 from fastapi import FastAPI, HTTPException
 
-port = config('RDS_HOST', default=None)
-host = config('RDS_PORT', default=5432, cast=int)
+host = config('RDS_HOST', default=None)
+port = config('RDS_PORT', default=5432, cast=int)
 user = config('RDS_USER', default=None)
 password = config('RDS_PASSWORD', default=None)
+database_name = config('RDS_DATABASE', default=None)
 
+class User(BaseModel):
+    username: str
+    password: str
+    zone: int
 
-#engine = create_engine("postgresql://usuario:contraseña@localhost/nombre_base_de_datos")
+DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{database_name}"
+print(DATABASE_URL)
+engine = create_engine(DATABASE_URL)
+print(engine)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+print(SessionLocal)
 
 app = FastAPI()
 
 @app.post("/auth/")
-async def autenticar(username: str, password: str, zone: str):
-    db = SessionLocal()
-    usuario_db = db.query(Usuario).filter(Usuario.nombre == username, Usuario.password == password, Usuario.zona == zone).first()
-    if not usuario_db:
-        raise HTTPException(status_code=401, detail="Credenciales inválidas")
-    return {"mensaje": "Autenticación exitosa"}
+async def autenticar(user: User):
+    try:
+        print(user)
+        print("Ok connection")
+        db = SessionLocal()
+        user_db = db.query(Users).filter(Users.username == username, Users.password == password).first()
+        print("Query ok")
+        if not user_db:
+            raise HTTPException(status_code=401, detail="Credenciales inválidas")
+
+        permisos = db.query(Permissions).filter(Permissions.id_user == user_db.id, Permissions.id_zone == zone).first()
+        if not permisos:
+            raise HTTPException(status_code=403, detail="El usuario no tiene permisos para acceder a esta zona")
+
+        return {"mensaje": "Autenticación exitosa"}
+    except Exception as e:
+        print("Error:", e)
+        raise HTTPException(status_code=500, detail="Error interno del servidor")
