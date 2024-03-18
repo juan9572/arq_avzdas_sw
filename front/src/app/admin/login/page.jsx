7c010042e8c16af6c63db04f10e739ccec17ce64
mvillegas2 <martin.villegas@ibox.com.co>
2024-03-17 23:14:40 -0500

    making login for admin

--- front/src/app/admin/login/page.jsx
+++ front/src/app/admin/login/page.jsx
@@ -1,3 +1,135 @@
+'use client'
+import axios from 'axios';
+import { useState } from 'react';
+import Alert from '../../components/Alert';
+import Swal from 'sweetalert2';
+
+const zonaAIngresar = process.env.NEXT_PUBLIC_ZONA_A_INGRESAR;
+
 export default function AdminLogin() {
-    return <div>AdminLogin</div>
+    const [credentials, setCredentials] = useState({
+        user: "",
+        password: "",
+        zone: zonaAIngresar,
+    });
+    const [alert, setAlert] = useState({
+        open: false,
+        icon: "",
+        title: "",
+        text: "",
+        location: "",
+        confirmButtonText: "Finalizar",
+        secondButtonText: null,
+    });
+
+    const authAdmin = async (e) => {
+        e.preventDefault();
+    }
+
+    const handleSecondButton = () => {
+        setAlert((prev) => ({
+            ...prev,
+            open: false,
+        }));
+    };
+    const closeAlert = () => {
+        setAlert((prev) => ({
+            ...prev,
+            open: false,
+        }));
+    };
+
+    const validateUser = async () => {
+        console.log('Validando usuario');
+        Swal.fire({
+            title: "Validando usuario",
+            timerProgressBar: true,
+            allowOutsideClick: false,
+            didOpen: () => {
+                Swal.showLoading();
+            }
+        });
+        try {
+            const { data } = await axios.post('/api/authAdmin', credentials);
+            console.log("hola")
+            console.log(data);
+            Swal.close()
+            setAlert((prev) => ({
+                ...prev,
+                icon: "success",
+                title: "Usuario validado",
+                text: "Puede ingresar a la zona",
+                open: true,
+                secondButtonText: null,
+                confirmButtonText: "Finalizar",
+                location: "/admin",
+            }));
+        } catch (err) {
+            Swal.close()
+            console.log(err);
+            setAlert((prev) => ({
+                ...prev,
+                icon: "error",
+                title: "Error",
+                text: "Usuario no valido",
+                open: true,
+                secondButtonText: null,
+                confirmButtonText: "Finalizar",
+                location: "/admin/login",
+            }));
+        }
+    }
+
+    const userChange = (e) => {
+        setCredentials((prev) => ({
+            ...prev,
+            user: e.target.value,
+        }));
+    }
+
+    const pasChange = (e) => {
+        setCredentials((prev) => ({
+            ...prev,
+            password: e.target.value,
+        }));
+    }
+
+
+
+    return (
+        <>
+            {alert.open && (
+                <Alert
+                    icon={alert.icon}
+                    title={alert.title}
+                    text={alert.text}
+                    location={alert.location}
+                    closeAlert={closeAlert}
+                    confirmButtonText={alert.confirmButtonText}
+                    secondButtonText={alert.secondButtonText}
+                    handleSecondButton={handleSecondButton}
+                />
+            )}
+            <main className="relative w-full h-[100vh]">
+                <div className="absolute inset-0">
+                    <img style={{ filter: 'brightness(50%)' }} className="w-full h-full" src="/bg.jpg" alt="" />
+                </div>
+                <div className="absolute left-[50%] top-[50%] z-10 w-[50%] h-[80%] rounded-lg bg-white" style={{ transform: 'translate(-50%, -50%)' }}>
+                    <form onSubmit={authAdmin} action="" className='flex flex-col items-center justify-center pt-16 p-8 gap-8 text-black'>
+                        <h1 className='font-bold text-3xl text-center'>Modulo de Administrador</h1>
+                        <h4 className='text-lg font-medium p-6 text-center'>Ingresa el usuario y la contraseña</h4>
+                        <input type="text" placeholder='User' onChange={userChange} value={credentials.user} className='h-12 w-[80%] text-center border-2'></input>
+                        <input type="password" placeholder='Password' onChange={pasChange} value={credentials.password} className='h-12 w-[80%] text-center border-2'
+                        />
+
+                        <button onClick={() => validateUser()} type="submit" style={{
+                            backgroundImage: `linear-gradient(to right, #0034e3, #00006d)`,
+                        }} className="w-72 h-20 text-white rounded-full font-semibold border-white border-2 mt-6">Ingresar</button>
+                    </form>
+
+                </div>
+            </main >
+
+        </>
+    )
 }
--- front/src/app/api/authAdmin/route.js
+++ front/src/app/api/authAdmin/route.js
@@ -1,13 +1,13 @@
 import { NextResponse } from "next/server";
 
-const Username = process.env.NEXT_PUBLIC_USERNAME;
-const Password = process.env.NEXT_PUBLIC_PASSWORD;
-const Zone = process.env.NEXT_PUBLIC_ZONE_ID;
+
 const Api = process.env.NEXT_PUBLIC_API;
 
 export async function POST(req, res) {
+    const response = await req.json();
+    console.log(response);
     try {
-
+        const { Username, Password, Zone } = response;
         const resAxios = await fetch(Api + ``, {
             body: JSON.stringify({
                 username: Username,
@@ -20,18 +20,21 @@ export async function POST(req, res) {
             method: "POST",
         });
 
-        // // // Espera respuesta real de Api
+        // // Simulate successful response from API
+        const token = "your_token_here";
+        const data = {
+            token: token,
+        };
+        return NextResponse.json(data, {
+            status: 200,
+        });
+        // Espera respuesta real de Api
         // const { data } = await resAxios.json();
         // return NextResponse.json({
         //     ...data,
         // });
 
-        //Simula respuesta de Api
-        const inventedData = [1, 2, 3, 4, 5];
-        return NextResponse.json({
-            // ...data,
-            inventedData,
-        });
+
     } catch (err) {
         console.log(err);
         return NextResponse.json(
